08dd7fa1eaa4fd195b0331548a8e9d85aa1d9907
naxieyun <497078141@qq.com>
2016-08-18 21:36:29 +0800

    添加演示GIF

--- app/src/main/java/yyl/windmill/MainActivity.java
+++ app/src/main/java/yyl/windmill/MainActivity.java
@@ -2,10 +2,15 @@ package yyl.windmill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private WindPath mBigWindMill,mSmallWindMill;
+
+    private SeekBar mSeekBar;
+    private TextView mSpeedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +19,32 @@ public class MainActivity extends AppCompatActivity {
 
         mBigWindMill = (WindPath) findViewById(R.id.id_wind);
         mSmallWindMill = (WindPath) findViewById(R.id.id_windsmall);
-        initWind();
-    }
-    private void initWind() {
-        mBigWindMill.setWindvelocity(3);
-        mSmallWindMill.setWindvelocity(3);
+        mSeekBar = (SeekBar) findViewById(R.id.id_seekbar);
+        mSpeedText = (TextView) findViewById(R.id.speed);
+        mSpeedText.setText(mSeekBar.getProgress() + "/" + mSeekBar.getMax());
         mBigWindMill.startAnim();
         mSmallWindMill.startAnim();
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mSpeedText.setText(seekBar.getProgress() + "/" + mSeekBar.getMax());
+
+                mBigWindMill.setWindvelocity(seekBar.getProgress());
+                mSmallWindMill.setWindvelocity(seekBar.getProgress());
+                mBigWindMill.startAnim();
+                mSmallWindMill.startAnim();
+            }
+        });
     }
 }
